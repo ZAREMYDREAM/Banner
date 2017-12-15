f88cb3a95da1ee064305a0f6e24f0f32f51cf6f2
ZAREMYDREAM <409808588@qq.com>
2017-12-15 17:07:19 +0800

    no message

--- demo1/ViewController.m
+++ demo1/ViewController.m
@@ -12,12 +12,14 @@
 
 @property(strong,nonatomic)UICollectionView *collection;
 
+@property(strong,nonatomic)UIPageControl *page;
+
 @property(strong,nonatomic)NSTimer *timer;
 
 @end
 
 @implementation ViewController
-
+#pragma mark - 懒加载
 -(UICollectionView *)collection{
     if (!_collection) {
         UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
@@ -35,26 +37,38 @@
         _collection.delegate = self;
         _collection.dataSource = self;
         
-        _collection.pagingEnabled = YES;
+        _collection.pagingEnabled = YES; //设置整页滚动
         
-        _collection.contentOffset = CGPointMake(self.view.frame.size.width, 0);
+        _collection.contentOffset = CGPointMake(self.view.frame.size.width, 0); //设置初始位置为第一张图
         
-      self.timer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(scrollImage) userInfo:nil repeats:YES];
+        _collection.showsHorizontalScrollIndicator = NO;//隐藏水平滚动条
+        
+      self.timer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(scrollImage) userInfo:nil repeats:YES]; //添加定时器 实现自动滚动
     }
     
     return _collection;
+}
+
+-(UIPageControl *)page{
+    if (!_page) {
+        _page = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
+        
+        _page.numberOfPages = 4; //设置圆点个数
+        
+        _page.currentPage = 0;//设置初始选中点
+        
+        _page.center = CGPointMake(self.view.frame.size.width / 2.0, 250);//设置中心位置
+    }
+    
+    return _page;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self.view addSubview:self.collection];
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self.view addSubview:self.page];
 }
 
 #pragma mark - UICollectionViewDelegate & UICollectionViewDataSource实现
@@ -80,6 +94,7 @@
         }
     }
     
+    //将第一张加到最后一个cell，将最后一张加载到最后一个cell
     NSInteger imageNum;
     switch (indexPath.row) {
         case 0:
@@ -105,6 +120,7 @@
 }
 
 #pragma mark - UIScrollViewDelegate实现
+//自动滚动结束时调用
 -(void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
     if ([scrollView isEqual:_collection])
     {
@@ -112,11 +128,46 @@
         
         if (page == 5)
         {
+            //当到达最后一个cell时，自动回到第二个cell
             [_collection setContentOffset:CGPointMake(self.view.frame.size.width, 0) animated:NO];
+            
+            self.page.currentPage = 0;
+        }
+        else if (page == 0)
+        {
+            //当到达第一个cell时，自动跳转到倒数第二个cell
+            [_collection setContentOffset:CGPointMake(5 * self.view.frame.size.width, 0) animated:NO];
+            
+            self.page.currentPage = 3;
+        }
+        else
+        {
+            self.page.currentPage = page - 1;
+        }
+    }
+}
+
+//手动拖动，滚动结束时调用
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    if ([scrollView isEqual:_collection])
+    {
+        int page = (int)_collection.contentOffset.x / self.view.frame.size.width;
+        
+        if (page == 5)
+        {
+            [_collection setContentOffset:CGPointMake(self.view.frame.size.width, 0) animated:NO];
+            
+            self.page.currentPage = 0;
         }
         else if (page == 0)
         {
             [_collection setContentOffset:CGPointMake(5 * self.view.frame.size.width, 0) animated:NO];
+            
+            self.page.currentPage = 3;
+        }
+        else
+        {
+            self.page.currentPage = page - 1;
         }
     }
 }
@@ -130,6 +181,7 @@
     if (page < 6)
     {
         [_collection setContentOffset:CGPointMake(page * self.view.frame.size.width, 0) animated:YES];
+        
     }
 
 }
